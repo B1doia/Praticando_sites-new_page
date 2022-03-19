895acbef2ee79edb9337496d0865e26f33598eae
Bidoia <83676738+B1doia@users.noreply.github.com>
2022-03-18 21:02:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-/*          *     .        *  .    *    *   . 
- .  *  move your mouse to over the stars   .
- *  .  .   change these values:   .  *
-   .      * .        .          * .       */
+
    const STAR_COLOR = '#fff';
    const STAR_SIZE = 3;
    const STAR_MIN_SCALE = 0.2;
